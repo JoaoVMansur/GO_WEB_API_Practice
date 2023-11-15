e74da38ce702d2c2574e570806610c1ca2a432c0
JoaoVMansur <jvictormansur@gmail.com>
2023-11-15 16:20:23 -0300

    modified main function to work with the new files structure

--- main.go
+++ main.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"os"
+	"web_service_gin/handlers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
-var db *sql.DB
-
 func main() {
-	var err error
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	cfg := mysql.Config{
-		User:                 os.Getenv("DBUSER"),
-		Passwd:               os.Getenv("DBPASS"),
-		Net:                  "tcp",
-		Addr:                 "127.0.0.1:3306",
-		DBName:               "recordings",
-		AllowNativePasswords: true,
-	}
-
-	db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
+	handlers.InitializeHandler()
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbumsId)
-	router.DELETE("/albums/:id", deleteAlbum)
-	router.PUT("/albums/:id", updateAlbum)
+	router.GET("/albums", handlers.GetAlbums)
+	router.POST("/albums", handlers.PostAlbums)
+	router.GET("/albums/:id", handlers.GetAlbumsId)
+	router.DELETE("/albums/:id", handlers.DeleteAlbum)
+	router.PUT("/albums/:id", handlers.UpdateAlbum)
 	router.Run("localhost:8080")
 
 }
